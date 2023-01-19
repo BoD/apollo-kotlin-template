7f007ca5c2665ab351c8e55d0c5dcde823532e45
BoD <BoD@JRAF.org>
2023-01-19 09:15:13 +0100

    Update to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.7.0"
-    id("com.apollographql.apollo3") version "3.4.1-SNAPSHOT"
+  id("com.apollographql.apollo3") version "3.7.4"
     application
 }
 
@@ -20,7 +20,7 @@ allprojects {
 
 
 dependencies {
-    implementation("com.apollographql.apollo3", "apollo-runtime", "3.4.1-SNAPSHOT")
+  implementation("com.apollographql.apollo3", "apollo-runtime")
 }
 
 apollo {
