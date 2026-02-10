a71c236f3e5ee31d68c4582ff93d5ee023915415
BoD <BoD@JRAF.org>
2026-02-10 11:46:01 +0100

    Setup test with 4.x

--- app/src/test/kotlin/Test.kt
+++ app/src/test/kotlin/Test.kt
@@ -1,18 +1,11 @@
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.Optional
-import com.apollographql.cache.normalized.FetchPolicy
-import com.apollographql.cache.normalized.api.NormalizedCache
-import com.apollographql.cache.normalized.apolloStore
-import com.apollographql.cache.normalized.fetchPolicy
 import com.apollographql.cache.normalized.memory.MemoryCacheFactory
 import com.apollographql.mockserver.MockServer
-import com.apollographql.mockserver.enqueueString
+import com.apollographql.mockserver.enqueueError
 import com.example.GetMessageQuery
-import com.example.SetMessageMutation
 import com.example.cache.Cache.cache
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class Test {
   @Test
@@ -23,32 +16,11 @@ class Test {
       .cache(MemoryCacheFactory(10 * 1024 * 1024))
       .build()
 
-    mockServer.enqueueString(
-      """
-      {
-        "data": {
-          "setMessage": {
-            "__typename": "Message",
-            "id": "0",
-            "message": "message0"
-          }
-        }
-      }
-      """.trimIndent()
-    )
-
-    val mutationResponse = apolloClient
-      .mutation(SetMessageMutation(Optional.present("message0")))
-      .fetchPolicy(FetchPolicy.CacheOnly)
-      .execute()
-    println(mutationResponse.data)
-    println(NormalizedCache.prettifyDump(apolloClient.apolloStore.dump()))
+    mockServer.enqueueError(500)
 
     val queryResponse = apolloClient
       .query(GetMessageQuery("0"))
-      .fetchPolicy(FetchPolicy.CacheOnly)
       .execute()
-    println(queryResponse.data)
-    assertEquals(mutationResponse.data!!.setMessage!!.id, queryResponse.data!!.message!!.id)
+    println(queryResponse.exception)
   }
 }
