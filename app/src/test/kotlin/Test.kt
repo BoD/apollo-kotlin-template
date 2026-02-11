feda10c71b4c402c86b487c43799e84fc8bcd9e3
BoD <BoD@JRAF.org>
2026-02-11 18:06:39 +0100

    Setup test with 4.x

--- app/src/test/kotlin/Test.kt
+++ app/src/test/kotlin/Test.kt
@@ -1,26 +1,13 @@
-import com.apollographql.apollo.ApolloClient
-import com.apollographql.cache.normalized.memory.MemoryCacheFactory
-import com.apollographql.mockserver.MockServer
-import com.apollographql.mockserver.enqueueError
-import com.example.GetMessageQuery
-import com.example.cache.Cache.cache
-import kotlinx.coroutines.test.runTest
+import com.example.UsersQuery
+import com.example.builder.Data
 import kotlin.test.Test
 
 class Test {
   @Test
-  fun test() = runTest {
-    val mockServer = MockServer()
-    val apolloClient = ApolloClient.Builder()
-      .serverUrl(mockServer.url())
-      .cache(MemoryCacheFactory(10 * 1024 * 1024))
-      .build()
-
-    mockServer.enqueueError(500)
-
-    val queryResponse = apolloClient
-      .query(GetMessageQuery("0"))
-      .execute()
-    println(queryResponse.exception)
+  fun test() {
+    val data2 = UsersQuery.Data {
+//      users = listOf(null)
+      this["users"] = listOf(null)
+    }
   }
 }
